ef1275d07f90c178d9992df25fa4ceddaac9c71e
cinglelas <linkaize1234@gmail.com>
2023-03-05 13:58:32 -0500

    pulling code 2

--- auto_scaler/app/main.py
+++ auto_scaler/app/main.py
@@ -6,15 +6,14 @@ sys.path.append('..')
 from configuration import backend_base_url, base_port
 
 
+active_node = 8
+max_miss_thres = 0.8
+min_miss_thres = 0.2
+expand_ratio = 2
+shrink_ratio = 0.5
 
 def checkMissRate():
-    ## should be retrieve from manager app
-    max_miss_thres = 0.8
-    min_miss_thres = 0.2
-    expand_ratio = 2
-    shrink_ratio = 0.5
-
-    active_node = 8
+    
     # get average miss rate from CloudWatch
     average_miss_rate = cw_api.getAverageMetric(active_node, seconds=60)
 
@@ -42,6 +41,20 @@ def checkMissRate():
 
     active_node = new_active_node
     print('current active node : ', active_node)
+
+@autoscaler.route('/update_params', methods=['GET'])
+def UpdateParams():
+    global active_node
+    global max_miss_thres
+    global min_miss_thres
+    global expand_ratio
+    global shrink_ratio
+
+    active_node = requests.form('active_node')
+    max_miss_thres = requests.form('max_miss_thres')
+    min_miss_thres = requests.form('min_miss_thres')
+    expand_ratio = requests.form('expand_ratio')
+    shrink_ratio = requests.form('expand_ratio')
 
 # scheduler to store statistics in database
 scheduler.add_job(func=checkMissRate, trigger='interval', seconds=10, id='job1')
--- run.sh
+++ run.sh
@@ -1,14 +1,83 @@
 #!/bin/bash
+
+function trap_ctrlc ()
+{
+    # perform cleanup here
+    echo "Ctrl-C caught...performing clean up"
+
+    echo "Doing cleanup"
+    kill -9 ${PID1}
+    kill -9 ${PID2}
+    kill -9 ${PID3}
+    kill -9 ${PID5001}
+    kill -9 ${PID5002}
+    kill -9 ${PID5003}
+    kill -9 ${PID5004}
+    kill -9 ${PID5005}
+    kill -9 ${PID5006}
+    kill -9 ${PID5007}
+    kill -9 ${PID5008}
+    # exit shell script with error code 2
+    # if omitted, shell script will continue execution
+    exit 2
+}
+trap "trap_ctrlc" 2
+
+
 cd frontend
 echo "before go through"
-python run.py & 
+python run.py &
+PID1=$!
+echo ${PID1}
 echo "manage app"
 cd ..
 cd manager_app
 python run.py &
+<<<<<<< HEAD
+=======
+PID2=$!
+echo ${PID2}
+cd react-manage-app
+yarn start &
+PID3=$!
+echo ${PID3}
+cd ..
+>>>>>>> 263ed80 (pulling code 2)
 cd ..
 cd memcache
-for i in {5001..5008}
-do
-    flask run --port $i &
-done
+#for i in {5001..5008}
+#do
+#    flask run --port $i &
+#    PID${i}=$!
+#done
+flask run --port 5001 &
+PID5001=$!
+echo ${PID5001}
+flask run --port 5002 &
+PID5002=$!
+echo ${PID5002}
+flask run --port 5003 &
+PID5003=$!
+echo ${PID5003}
+flask run --port 5004 &
+PID5004=$!
+echo ${PID5004}
+flask run --port 5005 &
+PID5005=$!
+echo ${PID5005}
+flask run --port 5006 &
+PID5006=$!
+echo ${PID5006}
+flask run --port 5007 &
+PID5007=$!
+echo ${PID5007}
+flask run --port 5008 &
+PID5008=$!
+echo ${PID5008}
+wait
+
+
+
+
+
+
